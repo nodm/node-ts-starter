5eb64028606723fb12766f83d4d6d233f996fc6a
Dmytro Novikov <nodm.pe@gmail.com>
2025-01-01 16:20:38 +0200

    Add dotenv support and update greeting function to use environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export default function greet(name = 'world'): void {
+import {getEnvVariable} from './utils';
+
+const defaultName = getEnvVariable('NAME', false);
+
+export default function run(name = defaultName): void {
   console.log(`Hello, ${name}!`);
 }
-greet();
+
+run();
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export {getEnvVariable} from './getEnvVariable';
