23cc76b8846ce7be790cfad405c436174a8f2cca
Dmytro Novikov <nodm.pe@gmail.com>
2025-07-27 08:15:17 +0300

    refactor: enhance getEnvVariable function with overloads and improve error message formatting

--- src/utils/getEnvVariable.ts
+++ src/utils/getEnvVariable.ts
@@ -1,12 +1,10 @@
 import 'dotenv/config';
 
-export function getEnvVariable(
-  key: string,
-  isOptional = false,
-): string | undefined {
+export function getEnvVariable(key: string): string;
+export function getEnvVariable(key: string, isOptional: true): string | undefined;
+export function getEnvVariable(key: string, isOptional = false): string | undefined {
   if (!isOptional && !Object.prototype.hasOwnProperty.call(process.env, key)) {
-    throw new Error(`Environment variable ${key} is not defined`);
+    throw new Error(`[getEnvVariable] Environment variable ${key} is not defined`);
   }
-
   return process.env[key];
 }
